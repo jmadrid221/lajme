235f603d198ca37ba7cad6af21b9e5d114a5332e
jmadrid221 <119139179+jmadrid221@users.noreply.github.com>
2022-12-02 20:45:20 +0100

    Update [[...slugs]].js

--- pages/posts/[[...slugs]].js
+++ pages/posts/[[...slugs]].js
@@ -51,7 +51,7 @@ export async function getStaticProps(Context) {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            url: 'https://fedly.net/' + slugString
+            url: 'https://rupalinews.net/' + slugString
         })
     })
 
